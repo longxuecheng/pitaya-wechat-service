5169d19c592cff5943e6406dfc4bc8528c8b39b4
longxuecheng <longxuecheng@meican.com>
2020-08-16 13:36:46 +0800

    Chore: APPID change

--- service/wechat/api.go
+++ service/wechat/api.go
@@ -7,9 +7,9 @@ import (
 )
 
 const (
-	appID              string = "wx91eb35616f475761"
-	secret             string = "41f8cb23aab151c6aa8ca64a0cfb596c"
-	merchantID         string = "1540271371"
+	appID              string = "wx84e045cf2c66a6db"
+	secret             string = "428dc00198ee5a146c0114b289d6087f"
+	merchantID         string = "1601779343"
 	authorize_url      string = "https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code"
 	accessToken_url    string = "https://api.weixin.qq.com/cgi-bin/token?grant_type=%s&appid=%s&secret=%s"
 	unifiedorder_url   string = "https://api.mch.weixin.qq.com/pay/unifiedorder"
